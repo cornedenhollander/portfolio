05583022a01010bc48c77bed6467fa537604ada3
Corné den Hollander <cornedenhollander@gmail.com>
2017-10-02 16:26:10 +0200

    Simplified page-top button function. Updated large screen usability.

--- js/main.js
+++ js/main.js
@@ -82,23 +82,4 @@ function main() {
     }
 }
 
-function topFunction() {
-    scrollTo(document.body, 0, 600);
-}
-var scrollme;
-scrollme = document.querySelector("#homeBtn");
-scrollme.addEventListener("click", topFunction, false)
-
-function scrollTo(element, to, duration) {
-    if (duration <= 0) return;
-    var difference = to - element.scrollTop;
-    var perTick = difference / duration * 10;
-
-    setTimeout(function () {
-        element.scrollTop = element.scrollTop + perTick;
-        if (element.scrollTop == to) return;
-        scrollTo(element, to, duration - 10);
-    }, 10);
-}
-
 main();
